370847876faa81716392875a7d2087ce38446a94
joe <joe@rstrike.my.id>
2025-03-23 14:21:27 +0000

    Initial commit

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/fern-wifi-cracker/issues
+# Bug-Submit: https://github.com/<user>/fern-wifi-cracker/issues/new
+# Changelog: https://github.com/<user>/fern-wifi-cracker/blob/master/CHANGES
+# Documentation: https://github.com/<user>/fern-wifi-cracker/wiki
+# Repository-Browse: https://github.com/<user>/fern-wifi-cracker
+# Repository: https://github.com/<user>/fern-wifi-cracker.git
